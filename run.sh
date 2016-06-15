aba146fe18630b444d3e34143b57acc1c457badc
Ajax <commial@gmail.com>
2016-06-15 18:00:41 +0200

    Adapt to new find.py API

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ echo "**************************************************************************
 echo "Run ARM L tests..."
 echo "********************************************************************************"
 
-python $SIBYL/find.py binaries/busybox-armv6l arml ABI_ARM $(cat addresses/busybox-armv6l_addr) -q 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-armv6l_result
+python $SIBYL/find.py -a arml -q binaries/busybox-armv6l ABI_ARM $(cat addresses/busybox-armv6l_addr) 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-armv6l_result
 
 echo "********************************************************************************"
 diff -u expected/busybox-armv6l_expected  busybox-armv6l_result
@@ -15,7 +15,7 @@ echo "**************************************************************************
 echo "Run MIPS32 L tests..."
 echo "********************************************************************************"
 
-python $SIBYL/find.py binaries/busybox-mipsel mips32l ABI_MIPS_O32 $(cat addresses/busybox-mipsel_addr) -q 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
+python $SIBYL/find.py  -a mips32l -q binaries/busybox-mipsel ABI_MIPS_O32 $(cat addresses/busybox-mipsel_addr) 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
 
 echo "********************************************************************************"
 diff -u expected/busybox-mipsel_expected  busybox-mipsel_result
