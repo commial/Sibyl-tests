0eb1c268da217cb27158d1808a2d40a7f5185d3a
Ajax <commial@gmail.com>
2016-06-15 18:07:40 +0200

    Additionnal tests with heuristics

--- run.sh
+++ run.sh
@@ -10,12 +10,26 @@ diff -u expected/busybox-armv6l_expected  busybox-armv6l_result
 echo "********************************************************************************"
 rm busybox-armv6l_result
 
+python $SIBYL/find.py -q binaries/busybox-armv6l ABI_ARM 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-armv6l_result
+
+echo "********************************************************************************"
+diff -u expected/busybox-armv6l_expected  busybox-armv6l_result
+echo "********************************************************************************"
+rm busybox-armv6l_result
+
 
 echo "********************************************************************************"
 echo "Run MIPS32 L tests..."
 echo "********************************************************************************"
 
-python $SIBYL/find.py  -a mips32l -q binaries/busybox-mipsel ABI_MIPS_O32 $(cat addresses/busybox-mipsel_addr) 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
+python $SIBYL/find.py -a mips32l -q binaries/busybox-mipsel ABI_MIPS_O32 $(cat addresses/busybox-mipsel_addr) 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
+
+echo "********************************************************************************"
+diff -u expected/busybox-mipsel_expected  busybox-mipsel_result
+echo "********************************************************************************"
+rm busybox-mipsel_result
+
+python $SIBYL/find.py -q binaries/busybox-mipsel ABI_MIPS_O32 2>&1 | grep -v "WARNING: " | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
 
 echo "********************************************************************************"
 diff -u expected/busybox-mipsel_expected  busybox-mipsel_result
