57313c3df86dc8b63a1e5061eab38d9eae9a9b29
Ajax <commial@gmail.com>
2016-10-05 13:54:35 +0200

    Merge duplicate code

--- run.sh
+++ run.sh
@@ -1,66 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 NOBUF="stdbuf -oL -eL"
+set -e
+set -o pipefail
+
+function run_test {
+	NAME=$1
+	ABI=$2
+	EXTRA_ARGS=$3
+
+	RESULT=$name\_result
+	($NOBUF python $SIBYL/find.py -q $EXTRA_ARGS binaries/$NAME $ABI $(cat addresses/$NAME\_addr) 2>&1 || echo 'Fail' 1>&2) | $NOBUF grep -Ev '(WARNING|access to non writable page)' | tee $RESULT
+
+	echo "********************************************************************************"
+	sort $RESULT -o $RESULT
+	diff -u expected/$NAME\_expected $RESULT || true
+	echo "********************************************************************************"
+	rm $RESULT
+}
 
 echo "********************************************************************************"
 echo "Run x86_32 tests..."
 echo "********************************************************************************"
 
-$NOBUF python $SIBYL/find.py -q -a x86_32 binaries/libc-2.21.so ABIStdCall_x86_32 $(cat addresses/libc-2.21_addr) 2>&1 | $NOBUF grep -Ev "(WARNING|access to non writable page)" | tee libc-2.21_result
+run_test libc-2.21.so ABIStdCall_x86_32 '-a x86_32'
 
-echo "********************************************************************************"
-sort libc-2.21_result -o libc-2.21_result
-diff -u expected/libc-2.21_expected  libc-2.21_result
-echo "********************************************************************************"
-rm libc-2.21_result
-
-python $SIBYL/find.py -q binaries/libc-2.21.so ABIStdCall_x86_32 2>&1 | grep -v "WARNING" | grep -v "access to non writable page" | sort | tee libc-2.21_result
-
-echo "********************************************************************************"
-diff -u expected/libc-2.21_expected  libc-2.21_result
-echo "********************************************************************************"
-rm libc-2.21_result
+run_test libc-2.21.so ABIStdCall_x86_32
 
 echo "********************************************************************************"
 echo "Run ARM L tests..."
 echo "********************************************************************************"
 
-python $SIBYL/find.py -a arml -q binaries/busybox-armv6l ABI_ARM $(cat addresses/busybox-armv6l_addr) 2>&1 | grep -v "WARNING" | grep -v "access to non writable page" | sort | tee busybox-armv6l_result
+run_test busybox-armv6l ABI_ARM '-a arml'
 
-echo "********************************************************************************"
-diff -u expected/busybox-armv6l_expected  busybox-armv6l_result
-echo "********************************************************************************"
-rm busybox-armv6l_result
-
-python $SIBYL/find.py -q binaries/busybox-armv6l ABI_ARM 2>&1 | grep -v "WARNING" | grep -v "access to non writable page" | sort | tee busybox-armv6l_result
-
-echo "********************************************************************************"
-diff -u expected/busybox-armv6l_expected  busybox-armv6l_result
-echo "********************************************************************************"
-rm busybox-armv6l_result
-
+run_test busybox-armv6l ABI_ARM
 
 echo "********************************************************************************"
 echo "Run MIPS32 L tests..."
 echo "********************************************************************************"
 
-python $SIBYL/find.py -a mips32l -q binaries/busybox-mipsel ABI_MIPS_O32 $(cat addresses/busybox-mipsel_addr) 2>&1 | grep -v "WARNING" | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
+run_test busybox-mipsel ABI_MIPS_O32 '-a mips32l'
+
+run_test busybox-mipsel ABI_MIPS_O32
 
 echo "********************************************************************************"
-diff -u expected/busybox-mipsel_expected  busybox-mipsel_result
-echo "********************************************************************************"
-rm busybox-mipsel_result
-
+echo "Run LEARN tests..."
 echo "********************************************************************************"
 cd learned_binaries
 python run_learn_tests.py 2> /dev/null
 cd ..
 echo "********************************************************************************"
-
-
-# Too slow for travis
-# python $SIBYL/find.py -q binaries/busybox-mipsel ABI_MIPS_O32 2>&1 | grep -v "WARNING" | grep -v "access to non writable page" | sort | tee busybox-mipsel_result
-# 
-# echo "********************************************************************************"
-# diff -u expected/busybox-mipsel_expected  busybox-mipsel_result
-# echo "********************************************************************************"
-# rm busybox-mipsel_result
